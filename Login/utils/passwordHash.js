6853e2fab79999379b4d4f3af0fc2e52cff422cc
Sumith1908 <sumith.hegde@iiitb.ac.in>
2024-05-24 11:10:09 +0530

    changes

--- Login/utils/passwordHash.js
+++ Login/utils/passwordHash.js
@@ -5,7 +5,7 @@ const { hash, compare, genSalt } = bcrypt;
 const generatePasswordHash = async (password) => {
   const salt = await genSalt();
   const hashPwd = await hash(password, salt);
-  return password;
+  return hashPwd;
 };
 
 const verifyPassword = async (password, pwdHash) => {
@@ -14,4 +14,4 @@ const verifyPassword = async (password, pwdHash) => {
   return data;
 };
 
-export default { generatePasswordHash, verifyPassword }; // Use module.exports instead of module.export
+module.exports = { generatePasswordHash, verifyPassword }; // Use module.exports instead of module.export
